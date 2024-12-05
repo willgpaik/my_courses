1faef00b0347746accea784de88bbb6f6393e593
Will Paik <bgh0919@gmail.com>
2024-12-04 20:27:08 -0500

    2024/12/04

---
+++ algorithm/linked_list/disp.c
@@ -0,0 +1,50 @@
+// display a link list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int *a, int n)
+{
+    struct Node *t, *last;
+    first = (struct Node*) malloc (sizeof(struct Node));
+    first->data = a[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node*) malloc (sizeof(struct Node));
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void disp(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
+    }
+}
+
+
+int main()
+{
+    int a[] = {3, 5, 7, 10, 15};
+
+    create(a, 5); // array and size
+
+    printf("\n");
+    disp(first);
+    printf("\n");
+    
+    return 0;
+}
---
+++ algorithm/linked_list/recursive_disp.c
@@ -0,0 +1,50 @@
+// display a link list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+void create(int *a, int n)
+{
+    struct Node *t, *last;
+    first = (struct Node*) malloc (sizeof(struct Node));
+    first->data = a[0];
+    first->next = NULL;
+    last = first;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = (struct Node*) malloc (sizeof(struct Node));
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Rdisp(struct Node *p)
+{
+    if (p != NULL)
+    {
+        printf("%d ", p->data);
+        Rdisp(p->next);
+    }
+}
+
+
+int main()
+{
+    int a[] = {3, 5, 7, 10, 15};
+
+    create(a, 5); // array and size
+
+    printf("\n");
+    Rdisp(first);
+    printf("\n");
+
+    return 0;
+}

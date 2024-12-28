2251483fa6708cdc6e460c3b1302cee58ee6db0b
willgpaik <bgh0919@gmail.com>
2024-12-28 16:59:48 -0500

    2024/12/28

---
+++ algorithm/linked_list/challenge2.cpp
@@ -0,0 +1,52 @@
+// find intersection of two linked list
+#include <iostream>
+#include <cmath>
+#include <stack>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+Node *head = new Node;
+
+void create(int *a, int n)
+{
+    Node *last, *t;
+    
+    head->data = a[0];
+    head->next = NULL;
+    last = head;
+
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = a[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void disp(Node *p)
+{
+    while (p)
+    {
+        std::cout << p->data << " ";
+        p = p->next;
+    }
+
+    std::cout << "\n";
+}
+
+
+int main()
+{
+    int a[] = {8, 6, 3, 9, 10, 4, 2};
+    create(a, 7);
+    disp(head);
+
+    return 0;
+}

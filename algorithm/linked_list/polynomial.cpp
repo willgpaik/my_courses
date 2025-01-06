3ca990760cd57ed1e0fdc0d308434d1c67916104
willgpaik <bgh0919@gmail.com>
2025-01-05 23:06:37 -0500

    2025/01/05

--- algorithm/linked_list/polynomial.cpp
+++ algorithm/linked_list/polynomial.cpp
@@ -1,5 +1,6 @@
 // polynomial representation
 #include <iostream>
+#include <cmath>
 
 class Node
 {
@@ -8,3 +9,70 @@ public:
     int exp;
     Node * next;
 };
+
+class Polynomial
+{
+private:
+    Node *poly;
+
+public:
+    Polynomial()
+    {
+        this->poly = NULL;
+    }
+    
+    ~Polynomial()
+    {
+        delete [] poly;
+    }
+
+    void create();
+    void disp();
+    void eval(int x);
+};
+
+void Polynomial::create()
+{
+    Node *t, *last;
+    int num;
+
+    std::cout << "enter number of terms: ";
+    std::cin >> num;
+    std::cout << "\nenter each term with coeff and exp\n";
+
+    for (int i = 0; i < num; i++)
+    {
+        t = new Node;
+        std::cin >> t->coeff >> t->exp;
+        t->next = NULL;
+
+        if (poly == NULL)
+            poly = last = t;
+        else
+        {
+            last->next = t;
+            last = t;
+        }
+    }
+}
+
+void Polynomial::disp()
+{
+    while (poly)
+    {
+        std::cout << poly->coeff << "x^" << poly->exp << " + ";
+        poly = poly->next;
+    }
+    std::cout << "\n";
+}
+
+
+int main()
+{
+    Polynomial a;
+    a.create();
+    a.disp();
+//    a.eval(1);
+
+    return 0;
+}

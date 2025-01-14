5f5e9c4764fe41b7a1bcf62ceb0156fee8a64d9f
willgpaik <bgh0919@gmail.com>
2025-01-13 23:05:00 -0500

    2025/01/13

--- algorithm/stack/infixToPostfix.cpp
+++ algorithm/stack/infixToPostfix.cpp
@@ -1,5 +1,6 @@
 // convert an equation from infx to prefix form
 #include <iostream>
+#include <cstring>
 
 class Node
 {
@@ -34,6 +35,7 @@ public:
     bool isFull();
     void push(char c);
     char pop();
+    char stackTop();
 };
 
 bool Stack::isEmpty()
@@ -60,7 +62,7 @@ bool Stack::isFull()
     }
 }
 
-void Stack::push(int x)
+void Stack::push(char x)
 {
     if (isFull())
         std::cout << "stack overflow\n";
@@ -92,12 +94,21 @@ char Stack::pop()
     return x;
 }
 
+char Stack::stackTop()
+{
+    if (isEmpty())
+        return -1;
+    else
+        return top->data;
+
+}
+
 bool isOperand(char x)
 {
-    if (x == '+' || x == '-' || x == '*' || x == '/')
-        return true;
-    else 
+    if (x == '+' || x == '-' || x == '*' || x == '/' || x == '^')
         return false;
+    else 
+        return true;
 }
 
 int precedence(char x)
@@ -106,6 +117,8 @@ int precedence(char x)
         return 1;
     else if (x == '*' || x == '/')
         return 2;
+    else if (x == '^')
+        return 3;
     
     return 0;
 }
@@ -114,8 +127,29 @@ char *toPostfix(char *infix)
 {
     Stack st;
 
-    char *postfix;
+    char *postfix = new char[strlen(infix)+1]; // need extra space for null
 
+    int i, j;
+    i = j = 0;
+    while (infix[i] != '\0')
+    {
+        char c = infix[i];
+
+        if (isOperand(c))
+            postfix[j++] = infix[i++];
+        else
+        {
+            if (precedence(c) > precedence(st.stackTop()))
+                st.push(infix[i++]);
+            else
+                postfix[j++] = st.pop();
+        }
+    }
+    
+    while (! st.isEmpty())
+        postfix[j++] = st.pop();
+
+    postfix[j] = '\0';
 
     return postfix;
 }
@@ -123,7 +157,7 @@ char *toPostfix(char *infix)
 
 int main()
 {
-    char *infix = "a+b*c-d/e";
+    char infix[] = "a+b*c-d/e";
     char *postfix = toPostfix(infix);
 
     std::cout << postfix << "\n";

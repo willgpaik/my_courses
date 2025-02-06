239e4158fb62eae5ca08777ff802baa5a6763cee
Will Paik <bgh0919@gmail.com>
2025-02-05 22:45:32 -0500

    2025/02/05

--- algorithm/bst/basicBST.cpp
+++ algorithm/bst/basicBST.cpp
@@ -20,6 +20,13 @@ public:
         root = nullptr;
     }
 
+    BST(int key)
+    {
+        root = new Node;
+        root->data = key;
+        root->left = root->right = nullptr;
+    }
+
     Node *getRoot()
     {
         return root;
@@ -27,6 +34,9 @@ public:
     void insert(int key);
     void inorder(Node *p);
     Node *search(int key);
+
+    Node *rInsert(Node *p, int key);
+    Node *rSearch(Node *p, int key);
 };
 
 void BST::insert(int key)
@@ -97,6 +107,37 @@ Node *BST::search(int key)
     return nullptr;
 }
 
+Node *BST::rInsert(Node *p, int key)
+{
+    Node *t;
+
+    if (p == NULL)
+    {
+        t = new Node;
+        t->data = key;
+        t->left = t->right = nullptr;
+        
+        return t;
+    }
+
+    if (key < p->data)
+        p->left = rInsert(p->left, key);
+    else if (key > p->data)
+        p->right = rInsert(p->right, key);
+
+    return p;
+}
+
+Node *BST::rSearch(Node *p, int key)
+{
+    if (key < p->data)
+        p = rSearch(p->left, key);
+    else if (key > p->data)
+        p =  rSearch(p->right, key);
+
+    return p;
+}
+
 
 int main()
 {
@@ -111,10 +152,31 @@ int main()
  
     // Inorder traversal
     bst.inorder(bst.getRoot());
-    std::cout << "\n";;
+    std::cout << "\n";
  
     // Search
     Node* tmp = bst.search(2);
+    if (tmp != nullptr)
+        std::cout << tmp->data << "\n";
+    else
+        std::cout << "element not found" << "\n";
+
+
+    // Recursion:
+    BST bst2(10);
+    // Insert
+//    bst2.rInsert(bst2.getRoot(), 10);
+    bst2.rInsert(bst2.getRoot(), 5);
+    bst2.rInsert(bst2.getRoot(), 20);
+    bst2.rInsert(bst2.getRoot(), 8);
+    bst2.rInsert(bst2.getRoot(), 30);
+
+    // Inorder traversal
+    bst2.inorder(bst2.getRoot());
+    std::cout << "\n";
+
+    // Search
+    tmp = bst2.search(10);
     if (tmp != nullptr)
         std::cout << tmp->data << "\n";
     else

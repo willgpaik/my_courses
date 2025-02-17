19a9009796fabc80a190671dd906e44e23c324cc
Will Paik <bgh0919@gmail.com>
2025-02-16 19:09:42 -0500

    2025/02/16

--- algorithm/heap/basicHeap.cpp
+++ algorithm/heap/basicHeap.cpp
@@ -1,5 +1,6 @@
 // insert an element in a heap
 #include <iostream>
+#include <vector>
 
 void insert1(int *arr, int n)
 {
@@ -12,23 +13,48 @@ void insert1(int *arr, int n)
         arr[i] = arr[i/2];
         i = i/2;
     }
+    
     arr[i] = tmp;
 }
 
-void disp(int *arr, int size)
+template <class T>
+void disp(T &arr, int size)
 {
     for (int i = 0; i < size; i++)
         std::cout << arr[i] << " ";
     std::cout << "\n";
 }
 
+void insert2(std::vector<int> &vec, int key)
+{
+    int i = vec.size();
+    vec.push_back(key);
+
+    while (i > 0 && key > vec[i/2])
+    {
+        vec[i] = vec[i/2];
+        i = i/2;
+    }
+
+    vec[i] = key;
+}
+
 
 int main()
 {
+    // array version
     int a[] = {45, 35, 15, 30, 10, 12, 6, 5, 20, 50};
     insert1(a, 9);
     disp(a, sizeof(a)/sizeof(a[0]));
     std::cout << "\n";
+
+    // vector version
+    std::vector<int> v = {45, 35, 15, 30, 10, 12, 6, 5, 20};
+    disp(v, v.size());
+    v.reserve(15);  // Reserve space for 15 elements
+
+    insert2(v, 50);
+    disp(v, v.size());
 
     return 0;
 }

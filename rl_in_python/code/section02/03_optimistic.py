009a0b37b7844e4ce80dd0898cc3fee0ea129059
willgpaik <bgh0919@gmail.com>
2025-02-25 22:54:31 -0500

    2025/02/25

--- rl_in_python/code/section02/03_optimistic.py
+++ rl_in_python/code/section02/03_optimistic.py
@@ -21,17 +21,16 @@ class Bandit:
   def __init__(self, p):
     # p: the win rate
     self.p = p
-    self.p_estimate = 100. # TODO
-    self.N = 1. # TODO
+    self.p_estimate = 100.
+    self.N = 1.
 
   def pull(self):
     # draw a 1 with probability p
     return np.random.random() < self.p
 
   def update(self, x):
-    # TODO
     self.N += 1.
-    self.p_estimate = ((self.N - 1)*self.p_estimate + x) / self.N # TODO
+    self.p_estimate = ((self.N - 1)*self.p_estimate + x) / self.N
 
 
 def experiment():
@@ -40,7 +39,7 @@ def experiment():
   rewards = np.zeros(NUM_TRIALS)
   for i in range(NUM_TRIALS):
     # use optimistic initial values to select the next bandit
-    j = np.argmax([b.p_estimate for b in bandits]) # TODO
+    j = np.argmax([b.p_estimate for b in bandits])
 
     # pull the arm for the bandit with the largest sample
     x = bandits[j].pull()

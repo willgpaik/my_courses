aa989d377023579baed13ba8e8b512fb3e8da7ca
willgpaik <bgh0919@gmail.com>
2025-03-01 03:47:32 -0500

    2025/03/01

--- rl_in_python/code/section02/05_bayesian.py
+++ rl_in_python/code/section02/05_bayesian.py
@@ -18,61 +18,61 @@ BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
 
 
 class Bandit:
-  def __init__(self, p):
-    self.p = p
-    self.a = # TODO
-    self.b = # TODO
-    self.N = 0 # for information only
+    def __init__(self, p):
+        self.p = p
+        self.a = # TODO
+        self.b = # TODO
+        self.N = 0 # for information only
 
-  def pull(self):
-    return np.random.random() < self.p
+    def pull(self):
+        return np.random.random() < self.p
 
-  def sample(self):
-    return # TODO - draw a sample from Beta(a, b)
+    def sample(self):
+        return # TODO - draw a sample from Beta(a, b)
 
-  def update(self, x):
-    self.a = # TODO
-    self.b = # TODO
-    self.N += 1
+    def update(self, x):
+        self.a = # TODO
+        self.b = # TODO
+        self.N += 1
 
 
 def plot(bandits, trial):
-  x = np.linspace(0, 1, 200)
-  for b in bandits:
-    y = beta.pdf(x, b.a, b.b)
-    plt.plot(x, y, label=f"real p: {b.p:.4f}, win rate = {b.a - 1}/{b.N}")
-  plt.title(f"Bandit distributions after {trial} trials")
-  plt.legend()
-  plt.show()
+    x = np.linspace(0, 1, 200)
+    for b in bandits:
+        y = beta.pdf(x, b.a, b.b)
+        plt.plot(x, y, label=f"real p: {b.p:.4f}, win rate = {b.a - 1}/{b.N}")
+    plt.title(f"Bandit distributions after {trial} trials")
+    plt.legend()
+    plt.show()
 
 
 def experiment():
-  bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
+    bandits = [Bandit(p) for p in BANDIT_PROBABILITIES]
 
-  sample_points = [5,10,20,50,100,200,500,1000,1500,1999]
-  rewards = np.zeros(NUM_TRIALS)
-  for i in range(NUM_TRIALS):
-    # Thompson sampling
-    j = # TODO
+    sample_points = [5,10,20,50,100,200,500,1000,1500,1999]
+    rewards = np.zeros(NUM_TRIALS)
+    for i in range(NUM_TRIALS):
+        # Thompson sampling
+        j = # TODO
 
-    # plot the posteriors
-    if i in sample_points:
-      plot(bandits, i)
+        # plot the posteriors
+        if i in sample_points:
+            plot(bandits, i)
 
-    # pull the arm for the bandit with the largest sample
-    x = bandits[j].pull()
+        # pull the arm for the bandit with the largest sample
+        x = bandits[j].pull()
 
-    # update rewards
-    rewards[i] = x
+        # update rewards
+        rewards[i] = x
 
-    # update the distribution for the bandit whose arm we just pulled
-    bandits[j].update(x)
+        # update the distribution for the bandit whose arm we just pulled
+        bandits[j].update(x)
 
-  # print total reward
-  print("total reward earned:", rewards.sum())
-  print("overall win rate:", rewards.sum() / NUM_TRIALS)
-  print("num times selected each bandit:", [b.N for b in bandits])
+    # print total reward
+    print("total reward earned:", rewards.sum())
+    print("overall win rate:", rewards.sum() / NUM_TRIALS)
+    print("num times selected each bandit:", [b.N for b in bandits])
 
 
 if __name__ == "__main__":
-  experiment()
+    experiment()
